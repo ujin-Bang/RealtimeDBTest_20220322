d267c9ce8e917848856c51dcf2059f367950ddd9
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-22 09:51:25 +0900

    Realtime firbase 라이브러리 설치

--- app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
+++ app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.realtimedbtest_20220322
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.realtimedbtest_20220322.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
@@ -14,6 +19,6 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-        
+
     }
 }
