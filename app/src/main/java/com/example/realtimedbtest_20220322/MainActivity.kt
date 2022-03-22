271721fdda640e669c48183d43b935a3c2aca881
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-22 09:45:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.realtimedbtest_20220322
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        
+    }
+}
