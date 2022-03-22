741434c38a4291115c19cdc24842222234101400
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-22 10:08:09 +0900

    채팅 UI 설정  - DB연결 정보를 BAseActivity에서 상속

--- app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
+++ app/src/main/java/com/example/realtimedbtest_20220322/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : BaseActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setupEvents()
         setValues()
     }
@@ -20,13 +20,6 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-//        DB연결 > 값 기록 연습
-        val db = FirebaseDatabase.getInstance("https://realtimedbtest-2022032200-default-rtdb.asia-southeast1.firebasedatabase.app/") // 싱가폴 DB주소 대입
 
-//        DB의 하위 항목 설정(Reference) 설정
-        val testRef = db.getReference("test")
-
-//        test항목에 "Hello World!!" 기록
-        testRef.setValue("Hello World")
     }
 }
